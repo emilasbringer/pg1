6918c85c0b0bb22e3a1a1661dd71ac9d47b00d99
emilasbringer <FattyRatty@users.noreply.github.com>
2020-10-23 11:00:25 +0200

    uppgift

---
+++ src/multiplikationstabell.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.Font;
+
+public class multiplikationstabell {
+    public static void main (String[] arg) {
+        UIManager.put("OptionPane.messageFont", new Font("Arial", Font.BOLD, 14));
+            /*
+            å = \u00E5
+            ä = \u00E4
+            ö = \u00F6
+            Ö = \u00D6
+            */
+
+        String a;
+        int b;
+        int c = 1;
+
+
+        a = JOptionPane.showInputDialog("Vad är 10:ans multiplikationstabell för:");
+        b = Integer.parseInt(a);
+
+        while(c<11)
+        {
+            System.out.println(c + "*" + b + "=" + b * c);
+            c = c+1;
+        }
+
+        }
+    }
